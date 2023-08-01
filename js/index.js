6950ec25641a2aa4d468728d26738e6e9fda818c
EvanilsonP <pevanilson99@gmail.com>
2023-08-01 11:19:17 -0300

    Mostrando quantidade de números apostados

--- js/index.js
+++ js/index.js
@@ -4,13 +4,22 @@ const btnApostar = document.getElementById('btnApostar');
 btnApostar.disabled = true;
 
 function selecionarNumeros(numero) {
-    // Limitando quantidade de escolhas dos números da megasena: Min 4 - max 15
+    // Limitando quantidade de escolhas dos números da megasena: Min 6 - max 15
     if(numerosApostados.length >= 0 && numerosApostados.length < 15) {
         numerosApostados.push(numero);
 
         // desabilita numero escolhido
         desabilitarNumeroEscolhido(numero);
     };
+
+    // Habilita o botão de apostar
+    if(numerosApostados.length > 5) {
+        btnApostar.disabled = false;
+    };
+
+    // mostrar quantidade de números apostados
+    const qtdApostas = document.getElementById("qtdNumeros");
+    qtdApostas.innerHTML = "<p>Qtd Números</p><p class='valor'>" + numerosApostados.length + "</p>";
 };
 
 function desabilitarNumeroEscolhido(numero) {
