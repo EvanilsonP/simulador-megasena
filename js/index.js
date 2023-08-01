aee4b6cfeeb0ef9667efc420a1e3757f5e157ee2
EvanilsonP <pevanilson99@gmail.com>
2023-08-01 11:05:18 -0300

    Limitando possibilidades de escolha de números

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 const numerosApostados = [];
 
 function selecionarNumeros(numero) {
-    // desabilita numero escolhido
-    desabilitarNumeroEscolhido(numero);
+    // Limitando quantidade de escolhas dos números da megasena: Min 4 - max 15
+    if(numerosApostados.length >= 0 && numerosApostados.length < 15) {
+        numerosApostados.push(numero);
+
+        // desabilita numero escolhido
+        desabilitarNumeroEscolhido(numero);
+    };
 };
 
 function desabilitarNumeroEscolhido(numero) {
